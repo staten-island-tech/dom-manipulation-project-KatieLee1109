001bee49ab74b0c7b9b6ba24dfca95bf0104153d
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2022-11-15 10:15:40 -0500

    Commit

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const DOMSelectors = {
     title: document.getElementById("title"),
-    artist: document.getElementById("thing"),
+    thing: document.getElementById("thing"),
     url: document.getElementById("url"),
     display: document.getElementById("display"),
 
 };
 
-const title = "things";
-console.log(DOMSelectors.title);
-const strawberries = {
-    title: "fruits",
-};
 
 
