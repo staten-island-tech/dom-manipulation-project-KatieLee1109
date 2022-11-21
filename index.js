db28bd4b5b0e53e28318309ad37d3986057fdd0b
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2022-11-21 10:16:31 -0500

    commit 9

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const DOMSelectors = {
-    title: document.getElementById("name"),
-    title2: document.getElementById("name2"),
-    text: document.getElementById("email"),
+    name: document.getElementById("name"),
+    name2: document.getElementById("name2"),
+    email: document.getElementById("email"),
     button: document.getElementById("btn"),
     form: document.getElementById("form"),
 };
 console.log(DOMSelectors);
 
 DOMSelectors.form.addEventListener("submit", function (submit) {
-    submit.defaultPrevented
+    submit.defaultPrevented();
 
-}
-)
-
-
-
-
-
-
-
-
-
-
-
+    let title = DOMSelectors.name.value;
+    let title2 = DOMSelectors.name2.value;
+    let text = DOMSelectors.email.value;
+    let button = DOMSelectors.btn.value;
+    console.log(DOMSelectors);
+    creation();
+    clear();
+    function creation() {
+        DOMSelectors.button.insertAdjacentHTML(
+            `afterend`,
+            `<div class="info">
+        <h2>your name is ${title} ${title2} and your email is ${text}.</h2>
+        <button class="clear">Clear</button>
+        </div>)`;
